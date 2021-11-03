a215d1a88ba0e6422fc4514c2ab2f26759bbf512
Nek-12 <vaizin.nikita@gmail.com>
2021-11-03 20:00:43 +0300

    swapped() for List

--- core-ktx/src/main/java/com/nek12/androidutils/extensions/core/common.kt
+++ core-ktx/src/main/java/com/nek12/androidutils/extensions/core/common.kt
@@ -2,7 +2,6 @@
 
 package com.nek12.androidutils.extensions.core
 
-
 import java.time.DayOfWeek
 import java.time.Instant
 import java.time.ZoneId
@@ -19,10 +18,10 @@ import kotlin.reflect.KProperty
  * @returns A copy of this list ordered according to the order
  */
 fun <T, R> Iterable<T>.reorderBy(order: List<R>, selector: (T) -> R): List<T> {
-    //associate the values with indexes and create a map
+    // associate the values with indexes and create a map
     val orderMap = order.withIndex().associate { it.value to it.index }
-    //sort the habits sorted using the comparator that places values not present in a map last
-    //and uses the order of the items in the map as its basis for sorting.
+    // sort the habits sorted using the comparator that places values not present in a map last
+    // and uses the order of the items in the map as its basis for sorting.
     return sortedWith(compareBy(nullsLast()) { orderMap[selector(it)] }).toMutableList()
 }
 
@@ -33,9 +32,17 @@ fun <T> MutableList<T>.swap(index1: Int, index2: Int): MutableList<T> {
     return this
 }
 
+/**
+ * Returns a shallow copy of this list with the items swapped
+ */
+fun <T> List<T>.swapped(index1: Int, index2: Int): List<T> {
+    val list = this.toMutableList()
+    return list.swap(index1, index2)
+}
+
 @Suppress("NewApi")
 fun Calendar.setDayOfWeek(dayOfWeek: DayOfWeek) {
-    //mapping 1=mon..7=sun -> 1=sun..7=mon
+    // mapping 1=mon..7=sun -> 1=sun..7=mon
     val mapped: Int = if (dayOfWeek.value == 7) 1 else dayOfWeek.value + 1
     set(Calendar.DAY_OF_WEEK, mapped)
 }
@@ -43,32 +50,31 @@ fun Calendar.setDayOfWeek(dayOfWeek: DayOfWeek) {
 /**
  * The count of digits in this [Int]
  */
-val Int.length get() = when (this) {
-    0 -> 1
-    else -> log10(abs(toDouble())).toInt() + 1
-}
+val Int.length
+    get() = when (this) {
+        0 -> 1
+        else -> log10(abs(toDouble())).toInt() + 1
+    }
 
 fun Boolean.toInt(): Int = if (this) 1 else 0
 
 @Suppress("NewApi")
-fun Instant.toZDT( zoneId: ZoneId = ZoneId.systemDefault()): ZonedDateTime = ZonedDateTime.ofInstant(this, zoneId)
+fun Instant.toZDT(zoneId: ZoneId = ZoneId.systemDefault()): ZonedDateTime = ZonedDateTime.ofInstant(this, zoneId)
 
 /**
  * This is an experimental implementation of a lazy property delegate that stores its state.
  * Mainly for extending other classes with lazy properties.
  */
-class LazyWithReceiver<This,Return>(val initializer:This.()->Return)
-{
-    private val values = WeakHashMap<This,Return>()
+class LazyWithReceiver<This, Return>(val initializer: This.() -> Return) {
+    private val values = WeakHashMap<This, Return>()
 
     @Suppress("UNCHECKED_CAST")
-    operator fun getValue(thisRef:Any,property:KProperty<*>):Return = synchronized(values)
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Return = synchronized(values)
     {
         thisRef as This
-        return values.getOrPut(thisRef) {thisRef.initializer()}
+        return values.getOrPut(thisRef) { thisRef.initializer() }
     }
 }
-
 
 /**
  * @return Whether this string is valid
