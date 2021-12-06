486bd35bb8360ac6f2151f27ecfd9b3f3ef8e0e9
Nek-12 <vaizin.nikita@gmail.com>
2021-12-06 15:35:17 +0300

    more linkTypes

--- android-ktx/src/main/java/com/nek12/androidutils/extensions/android/net.kt
+++ android-ktx/src/main/java/com/nek12/androidutils/extensions/android/net.kt
@@ -4,14 +4,33 @@ import android.net.Uri
 
 val Uri.asHttps: Uri get() = if (scheme == "http") buildUpon().scheme("https").build() else this
 val String?.asUri get() = this?.let { Uri.parse(it) }
+
 val Uri.linkType
     get() = when (this.scheme) {
+        null -> LinkType.Unknown
         "http", "https" -> LinkType.Web
         "mailto" -> LinkType.Mail
-        null -> LinkType.Unknown
+        "tel" -> LinkType.Tel
+        "blob" -> LinkType.Blob
+        "content" -> LinkType.ContentProvider
+        "dns" -> LinkType.Dns
+        "drm" -> LinkType.Drm
+        "fax" -> LinkType.Fax
+        "geo" -> LinkType.Geo
+        "magnet" -> LinkType.Magnet
+        "maps" -> LinkType.Map
+        "market" -> LinkType.GooglePlay
+        "messgage" -> LinkType.AppleMail
+        "mms", "sms" -> LinkType.TextMessage
+        "query" -> LinkType.FilesystemQuery
+        "resource" -> LinkType.Resource
+        "skype", "callto" -> LinkType.Skype
+        "ssh" -> LinkType.Ssh
+        "webcal" -> LinkType.Calendar
+        "file" -> LinkType.File
         else -> LinkType.Other
     }
 
 enum class LinkType {
-    Web, Mail, Other, Unknown
+    Web, Mail, Tel, Other, Unknown, Blob, ContentProvider, Dns, Drm, File, Calendar, Ssh, Skype, Resource, FilesystemQuery, TextMessage, AppleMail, GooglePlay, Map, Magnet, Geo, Fax
 }
