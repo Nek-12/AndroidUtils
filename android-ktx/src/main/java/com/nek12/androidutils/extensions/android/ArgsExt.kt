71ef19d230d0a999cd6c7995bb46aa04c7e419e5
Nek-12 <vaizin.nikita@gmail.com>
2022-11-11 12:35:02 +0300

    remove deprecated AlarmHelper, add new bundle functions and android functions
    Signed-off-by: Nek-12 <vaizin.nikita@gmail.com>

--- android-ktx/src/main/java/com/nek12/androidutils/extensions/android/ArgsExt.kt
+++ android-ktx/src/main/java/com/nek12/androidutils/extensions/android/ArgsExt.kt
@@ -3,8 +3,11 @@ package com.nek12.androidutils.extensions.android
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.core.os.bundleOf
+import java.io.Serializable
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 import kotlin.reflect.KProperty1
@@ -52,6 +55,30 @@ abstract class BundleExtra<in T, V>(
 @Suppress("ClassName")
 private object UNINITIALIZED_VALUE
 
-@Deprecated("Use the Tiramisu bundle type-safe api")
+@Deprecated("Use the Tiramisu bundle type-safe api (bundle.serializable)")
 inline fun <reified T> Intent.requireExtra(key: String) =
     requireNotNull(extras?.get(key)) { "required extra \"$key\" not provided" } as T
+
+@Suppress("DEPRECATION")
+inline fun <reified T : Serializable> Bundle.serializable(key: String) =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+        getSerializable(key, T::class.java)
+    else getSerializable(key) as? T?
+
+inline fun <reified T : Serializable> Intent.serializable(key: String): T? = extras?.serializable<T>(key)
+
+inline fun <reified T : Serializable> Bundle.requireSerializable(key: String): T = requireNotNull(serializable<T>(key))
+
+inline fun <reified T : Serializable> Intent.requireSerializable(key: String): T = requireNotNull(serializable<T>(key))
+
+@Suppress("DEPRECATION")
+inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+        getParcelable(key, T::class.java)
+    else getParcelable(key) as? T?
+
+inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = extras?.parcelable<T>(key)
+
+inline fun <reified T : Parcelable> Bundle.requireParcelable(key: String): T = requireNotNull(parcelable<T>(key))
+
+inline fun <reified T : Parcelable> Intent.requireParcelable(key: String): T = requireNotNull(parcelable<T>(key))
