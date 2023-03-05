8d92a362ce92a636793946b61013f78ac6883f41
Nek-12 <vaizin.nikita@gmail.com>
2023-03-05 23:13:29 +0300

    bump version
    Signed-off-by: Nek-12 <vaizin.nikita@gmail.com>

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -11,7 +11,7 @@ object Config {
     const val minSdk = 22
     const val compileSdk = 33
     val jvmTarget = JvmTarget.JVM_11
-    const val version = "1.0.1"
+    const val version = "1.0.2"
 
     val kotlinCompilerArgs = listOf(
         "-opt-in=kotlin.RequiresOptIn",
