b62ecd570e9ef51136dd17256a4cddf86841e41a
Nek-12 <vaizin.nikita@gmail.com>
2024-03-10 15:07:34 +0300

    deprecate Stable wrapper
    Signed-off-by: Nek-12 <vaizin.nikita@gmail.com>

--- compose-ktx/src/main/java/com/nek12/androidutils/compose/Stable.kt
+++ compose-ktx/src/main/java/com/nek12/androidutils/compose/Stable.kt
@@ -7,8 +7,10 @@ import androidx.compose.runtime.Immutable
  * Often used for 3rd-party dependencies that cannot be marked as `@Immutable` but are guaranteed to adhere to the
  * contract
  */
+@Deprecated("Use the Compose Stability Configuration File")
 @JvmInline
 @Immutable
 value class Stable<T>(val value: T)
 
+@Deprecated("Use the Compose Stability Configuration File")
 val <T> T.stable get() = Stable(this)
